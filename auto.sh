e2fe3e6502099c799bfc733fd79259760b68bc43
Ravi Kilnake <165386823+kilnake@users.noreply.github.com>
2024-04-30 16:24:18 +0200

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir -p /data/media/tv /data/media/music /data/media/movies /data/torrents/tv /data/torrents/music /data/torrents/movies /arr
+chown -R $USER:$USER /data /arr
+chmod -R 777 /data /arr
+cd /arr
+wget -q -O docker-compose.yml https://raw.githubusercontent.com/kilnake/proxmox/main/docker-compose.yml
+docker compose up -d
