a1338a0dac0175524aaea65dc2d1b28bea22c998
Ravi Kilnake <165386823+kilnake@users.noreply.github.com>
2024-05-02 12:22:46 +0200

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /data/media/tv /data/media/music /data/media/movies /data/torrents/tv /data/torrents/music /data/torrents/movies /arr
 chown -R $USER:$USER /data /arr
-chmod -R 777 /data /arr
+chmod -R 755 /data /arr
 cd /arr
 wget -q -O docker-compose.yml https://raw.githubusercontent.com/kilnake/proxmox/main/docker-compose.yml
 docker compose up -d
