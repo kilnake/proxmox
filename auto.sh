2e7b671e26e0a98dfabf84998f3b10451fff1221
Ravi Kilnake <165386823+kilnake@users.noreply.github.com>
2024-05-08 18:36:22 +0200

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,29 +1,5 @@
 #!/bin/sh
 
-# Stop and disable NetworkManager
-systemctl stop NetworkManager
-systemctl disable NetworkManager
-
-# Configure static IPv4 address
-cat <<EOF > /etc/network/interfaces
-auto lo
-iface lo inet loopback
-
-auto eth0
-iface eth0 inet static
-    address 192.168.1.20
-    netmask 255.255.255.0
-    gateway 192.168.1.1
-EOF
-
-# Configure static IPv6 address and gateway
-echo "iface eth0 inet6 static" >> /etc/network/interfaces
-echo "    address 2001:9b1:cdc0:a300::20/128" >> /etc/network/interfaces
-echo "    gateway 2001:9b1:cdc0:a300::1" >> /etc/network/interfaces
-
-# Restart networking service
-service networking restart
-
 # Create directories and set permissions
 mkdir -p /data/media/tv /data/media/music /data/media/movies /data/torrents/tv /data/torrents/music /data/torrents/movies /arr /arr/homepage /arr/homepage 
 chown -R $USER:$USER /data /arr
