5ae871267c097023332b1a25ec5e6ab2ce5664d0
Ravi Kilnake <165386823+kilnake@users.noreply.github.com>
2024-06-06 12:48:31 +0200

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
 # Create directories and set permissions
-mkdir -p /data/media/tv /data/media/music /data/media/movies /data/torrents/tv /data/torrents/music /data/torrents/movies /arr /arr/homepage
+mkdir -p /data/media/tv /data/media/music /data/media/movies /data/torrents/tv /data/torrents/music /data/torrents/movies /arr /arr/homepage /arr/prowlarr /arr/radarr /arr/sonarr
 chown -R $USER:$USER /data /arr
 chmod -R 777 /data /arr
 
 # Download configuration files
 cd /arr/homepage
 wget -q -O services.yaml https://raw.githubusercontent.com/kilnake/proxmox/main/arr/homepage/services.yaml
+cd /arr/prowlarr
+wget -q -O config.xml https://raw.githubusercontent.com/kilnake/proxmox/main/arr/prowlarr/config.xml
+cd /arr/radarr
+wget -q -O config.xml https://raw.githubusercontent.com/kilnake/proxmox/main/arr/radarr/config.xml
+cd /arr/sonarr
+wget -q -O config.xml https://raw.githubusercontent.com/kilnake/proxmox/main/arr/sonarr/config.xml
 cd /arr
 wget -q -O docker-compose.yml https://raw.githubusercontent.com/kilnake/proxmox/main/docker-compose.yml
 
