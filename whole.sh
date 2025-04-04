e92400af1be49df8cb7971151903e25abcafc3f9
Ravi Kilnake <165386823+kilnake@users.noreply.github.com>
2025-04-04 17:12:20 +0200

    Update whole.sh

--- whole.sh
+++ whole.sh
@@ -12,3 +12,7 @@ sed -i '/^#/d' "/etc/pve/lxc/$CT_ID.conf"
 DESCRIPTION="http://192.168.1.33:8989 | http://192.168.1.33:7878 | http://192.168.1.33:9696 | http://192.168.1.33:8191 | http://192.168.1.33:8096 | http://192.168.1.33:8998 | http://192.168.1.33:4444 | http://192.168.1.33:5055 | http://192.168.1.33:3080 | http://192.168.1.33:8080"
 
 echo -e "\n# $DESCRIPTION" >> "/etc/pve/lxc/$CT_ID.conf"
+
+pct enter $(ls -t /etc/pve/lxc/ | head -n1 | sed 's/\.conf//')
+
+pct enter "$CT_ID"
